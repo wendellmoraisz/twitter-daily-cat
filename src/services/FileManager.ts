59c48f959f60e75bd75b53c162afdc6b113e9e7a
wendellmoraisz <wendellmorais3319@gmail.com>
2023-01-22 11:47:39 -0300

    refactor: removing unnecessary explicit types

--- src/services/FileManager.ts
+++ src/services/FileManager.ts
@@ -2,16 +2,16 @@ const request = require("request");
 const fs = require("fs");
 
 class FileManager {
-  public downloadAndSaveImage(
+  public async downloadAndSaveImage(
     imgURL: string,
     destinyPath: string,
     callback?: () => void
-  ): void {
-    request.head(imgURL, () => {
-      request(imgURL)
-        .pipe(fs.createWriteStream(destinyPath))
-        .on("close", callback);
-    });
+  ) {
+    try {
+      request.head(imgURL, () => {
+        request(imgURL).pipe(fs.createWriteStream(destinyPath)).on("close", callback);
+      });
+    } catch (error) {}
   }
 }
 
--- src/services/PostMessageWriter.ts
+++ src/services/PostMessageWriter.ts
@@ -1,15 +1,15 @@
 class PostMessageWriter {
-  private static referenceDate: Date = new Date("03-05-2022");
+  private static referenceDate = new Date("03-05-2022");
 
-  public static getPostMessage(): string {
+  public static getPostMessage() {
     return this.formatMessageToUrl(`${this.getDaysQuantity()} dias`);
   }
 
-  private static formatMessageToUrl(message: string): string {
+  private static formatMessageToUrl(message: string) {
     return message.replaceAll(" ", "%20");
   }
 
-  private static getDaysQuantity(): number {
+  private static getDaysQuantity() {
     return Math.ceil(
       (Number(new Date()) - Number(this.referenceDate)) / (1000 * 60 * 60 * 24)
     );
